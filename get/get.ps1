997cc61606c2466efbe55505434adb833d4600d3
Barış Torunlar <32782265+baristorunlar@users.noreply.github.com>
2025-07-28 22:13:05 +0300

    Delete get directory

--- get/get.ps1
+++
@@ -1,3 +0,0 @@
-$path = "$env:TEMP\test.bat"
-Invoke-WebRequest -Uri "https://raw.githubusercontent.com/baristorunlar/KolayMenu/main/KolayMenu.bat" -OutFile $path
-Start-Process "cmd.exe" -ArgumentList "/c `"$path`""
