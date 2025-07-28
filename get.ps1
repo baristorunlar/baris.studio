e1cfc6e31db6651472b6dcc6b16e585c0b8946a1
Barış Torunlar <32782265+baristorunlar@users.noreply.github.com>
2025-07-28 22:57:29 +0300

    Update get.ps1

--- get.ps1
+++ get.ps1
@@ -1,3 +1,8 @@
-$path = "$env:TEMP\test.bat"
+# Geçici klasöre .bat dosyasını kaydet
+$path = "$env:TEMP\KolayMenu.bat"
+
+# BAT dosyasını GitHub'dan indir
 Invoke-WebRequest -Uri "https://raw.githubusercontent.com/baristorunlar/KolayMenu/main/KolayMenu.bat" -OutFile $path
+
+# Komut istemcisinde çalıştır
 Start-Process "cmd.exe" -ArgumentList "/c `"$path`""
