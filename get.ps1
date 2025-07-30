2352044842af4281becd4fcb2581bc6995f8cc57
Barış Torunlar <32782265+baristorunlar@users.noreply.github.com>
2025-07-30 19:57:52 +0300

    Update get.ps1

--- get.ps1
+++ get.ps1
@@ -1,3 +1,3 @@
 $path = "$env:TEMP\test.bat"
-Invoke-WebRequest -Uri "https://raw.githubusercontent.com/baristorunlar/KolayMenu/main/KolayMenu.bat" -OutFile $path
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/baristorunlar/baris.studio/refs/heads/main/data/KolayMen%C3%BC.bat" -OutFile $path
 Start-Process "cmd.exe" -ArgumentList "/c `"$path`""
